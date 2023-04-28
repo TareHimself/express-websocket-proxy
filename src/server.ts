60fc734f657d528a6391a9b12762582fcdca1adf
Tare Ebelo <75279482+TareHimself@users.noreply.github.com>
2023-04-28 05:52:41 -0400

    switch to raw websocket

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,43 @@
-import { ProxiedResponse, ProxyIdentify, RawProxiedRequest, ServerEvents, ServerStartOptions } from "./types";
-import express, { Application, Request as ExpressRequest, Response as ExpressResponse } from 'express';
+import {
+	ProxiedResponse,
+	ProxyIdentify,
+	RawProxiedRequest,
+	ServerEvents,
+	ServerStartOptions,
+} from './types';
+import express, {
+	Application,
+	Request as ExpressRequest,
+	Response as ExpressResponse,
+} from 'express';
 import http from 'http';
-import https from 'https'
-import { PROXY_PATH_REGEX, SOCKET_PROXIED_REQUEST_CLOSE, SOCKET_PROXIED_RESPONSE } from "./constants";
-import { Server as SocketIoServer, Socket } from "socket.io";
+import https from 'https';
+import {
+	PROXY_PATH_REGEX,
+	SOCKET_PROXIED_REQUEST_CLOSE,
+	SOCKET_PROXIED_RESPONSE,
+} from './constants';
+import { Server as SocketIoServer, Socket } from 'socket.io';
 import { v4 as uuidv4 } from 'uuid';
-import EventEmitter from "events";
-import { ParamsDictionary } from "express-serve-static-core";
-import { ParsedQs } from "qs";
-import { DefaultEventsMap } from "socket.io/dist/typed-events";
+import EventEmitter from 'events';
+import { ParamsDictionary } from 'express-serve-static-core';
+import { ParsedQs } from 'qs';
+import { DefaultEventsMap } from 'socket.io/dist/typed-events';
 
 export const DEFAULT_SERVER_OPTIONS: ServerStartOptions = {
 	port: 80,
-	use_ssl: false
-}
+	use_ssl: false,
+};
 
 export interface ServerOptions {
 	timeout: number;
 	debug: boolean;
-	shouldBindOnClose: (req: ExpressRequest, socket: Socket, method: string, payload: Partial<RawProxiedRequest>) => Promise<boolean>;
+	shouldBindOnClose: (
+		req: ExpressRequest,
+		socket: Socket,
+		method: string,
+		payload: Partial<RawProxiedRequest>
+	) => Promise<boolean>;
 	port: number;
 	use_ssl: boolean;
 	hostname?: string;
@@ -31,10 +50,17 @@ const DEFAULT_SERVER_OPTS: ServerOptions = {
 	debug: false,
 	shouldBindOnClose: async () => true,
 	port: 80,
-	use_ssl: false
+	use_ssl: false,
+};
+
+class ClientConnection {
+	constructor() {}
 }
 
-export type ExpressCallback = (req: ExpressRequest, res: ExpressResponse) => Promise<void>
+export type ExpressCallback = (
+	req: ExpressRequest,
+	res: ExpressResponse
+) => Promise<void>;
 
 export class Server<IdentifyType extends ProxyIdentify = ProxyIdentify> {
 	app: Application;
@@ -52,11 +78,11 @@ export class Server<IdentifyType extends ProxyIdentify = ProxyIdentify> {
 		this.app = express();
 		this.server = null;
 		this.socket = null;
-		this.callbacks_to_sockets = new Map()
-		this.sockets_to_callbacks = new Map()
-		this.authenticated_sockets = new Map()
-		this._emitter = new EventEmitter()
-		this.authenticator = (Socket: Socket, data: IdentifyType) => true
+		this.callbacks_to_sockets = new Map();
+		this.sockets_to_callbacks = new Map();
+		this.authenticated_sockets = new Map();
+		this._emitter = new EventEmitter();
+		this.authenticator = (Socket: Socket, data: IdentifyType) => true;
 	}
 
 	on<T extends keyof ServerEvents>(event: T, callback: ServerEvents[T]) {
@@ -71,71 +97,97 @@ export class Server<IdentifyType extends ProxyIdentify = ProxyIdentify> {
 		return this._emitter.off(event, callback);
 	}
 
-	emit<T extends keyof ServerEvents>(event: T, ...data: Parameters<ServerEvents[T]>) {
-		return this._emitter.emit(event, ...data)
+	emit<T extends keyof ServerEvents>(
+		event: T,
+		...data: Parameters<ServerEvents[T]>
+	) {
+		return this._emitter.emit(event, ...data);
 	}
 
-	handleSocket(req: ExpressRequest, res: ExpressResponse, socket: Socket, method: string, payload: Partial<RawProxiedRequest>, timeout = 10000) {
+	handleSocket(
+		req: ExpressRequest,
+		res: ExpressResponse,
+		socket: Socket,
+		method: string,
+		payload: Partial<RawProxiedRequest>,
+		timeout = 10000
+	) {
 		return new Promise<void>(async (resolve, reject) => {
-			let requestCompleted = false
-			const responseId = `${uuidv4()}`.replaceAll("-", '')
+			let requestCompleted = false;
+			const responseId = `${uuidv4()}`.replaceAll('-', '');
 
-			const start = Date.now()
+			const start = Date.now();
 
 			const onSocketResponse = (response: ProxiedResponse) => {
 				if (requestCompleted) return;
-				if (this.opts.debug) console.log(responseId, `<< Received after ${Date.now() - start}ms. Data :`, response)
+				if (this.opts.debug)
+					console.log(
+						responseId,
+						`<< Received after ${Date.now() - start}ms. Data :`,
+						response
+					);
 				requestCompleted = true;
-				req.off('close', onRequestClosed)
+				req.off('close', onRequestClosed);
 				if (response.headers) {
-					res.set(response.headers)
+					res.set(response.headers);
 				}
 
 				if (response.body === null && response.status !== null) {
-					res.sendStatus(response.status)
-				}
-				else {
-					if (response.status !== null) res.status(response.status)
+					res.sendStatus(response.status);
+				} else {
+					if (response.status !== null) res.status(response.status);
 					res.send(response.body);
 				}
 
-
-				resolve()
-			}
+				resolve();
+			};
 
 			const onRequestClosed = () => {
-
 				if (!requestCompleted) {
-					if (this.opts.debug) console.log(responseId, `<< Closed after ${Date.now() - start}ms`)
-					socket.off(`${responseId}|${SOCKET_PROXIED_RESPONSE}`, onSocketResponse)
-					socket.emit(`${responseId}|${SOCKET_PROXIED_REQUEST_CLOSE}`)
+					if (this.opts.debug)
+						console.log(responseId, `<< Closed after ${Date.now() - start}ms`);
+					socket.off(
+						`${responseId}|${SOCKET_PROXIED_RESPONSE}`,
+						onSocketResponse
+					);
+					socket.emit(`${responseId}|${SOCKET_PROXIED_REQUEST_CLOSE}`);
 				}
 
-				req.off('close', onRequestClosed)
-				resolve()
-			}
+				req.off('close', onRequestClosed);
+				resolve();
+			};
 
-			socket.on(`${responseId}|${SOCKET_PROXIED_RESPONSE}`, onSocketResponse)
-			if (this.opts.debug) console.log(responseId, ">> Waiting for response ",)
+			socket.on(`${responseId}|${SOCKET_PROXIED_RESPONSE}`, onSocketResponse);
+			if (this.opts.debug) console.log(responseId, '>> Waiting for response ');
 
-			if (await this.opts.shouldBindOnClose(req, socket, method, payload)) req.on('close', onRequestClosed)
-			socket.emit(method, { ...payload, id: responseId })
-		})
-
+			if (await this.opts.shouldBindOnClose(req, socket, method, payload))
+				req.on('close', onRequestClosed);
+			socket.emit(method, { ...payload, id: responseId });
+		});
 	}
 
 	async registerRoutes(routes: string[], socketId: string) {
-		if (this.opts.debug) console.log("Registering routes", routes, "from socket with id", socketId)
-		this.emit("CLIENT_CONNECT", socketId, routes);
-		const callbacks = routes.map(route => {
-			const [combined, method, path] = Array.from(PROXY_PATH_REGEX.exec(route)!)
+		if (this.opts.debug)
+			console.log(
+				'Registering routes',
+				routes,
+				'from socket with id',
+				socketId
+			);
+		this.emit('CLIENT_CONNECT', socketId, routes);
+		const callbacks = routes.map((route) => {
+			const [combined, method, path] = Array.from(
+				PROXY_PATH_REGEX.exec(route)!
+			);
 			const callback = async (req: ExpressRequest, res: ExpressResponse) => {
-				const funcHandle = req.route.stack[0].handle
-				if (!this.socket) throw new Error('Main Io Server is invalid')
-				if (this.opts.debug) console.log("\nProcessing request on route", route)
-				const socket = this.socket.sockets.sockets.get(this.callbacks_to_sockets.get(funcHandle)!)
+				const funcHandle = req.route.stack[0].handle;
+				if (!this.socket) throw new Error('Main Io Server is invalid');
+				if (this.opts.debug)
+					console.log('\nProcessing request on route', route);
+				const socket = this.socket.sockets.sockets.get(
+					this.callbacks_to_sockets.get(funcHandle)!
+				);
 				if (socket && socket.connected) {
-
 					const payload: RawProxiedRequest = {
 						params: req.params,
 						query: req.query,
@@ -144,92 +196,98 @@ export class Server<IdentifyType extends ProxyIdentify = ProxyIdentify> {
 						method: req.method,
 						originalUrl: req.originalUrl,
 						baseUrl: req.baseUrl,
-						path: req.path
-					} as unknown as RawProxiedRequest
-					await this.handleSocket(req, res, socket, route, payload)
+						path: req.path,
+					} as unknown as RawProxiedRequest;
+					await this.handleSocket(req, res, socket, route, payload);
 					return;
+				} else {
+					if (this.opts.debug)
+						console.log('Failed to reach socket', socketId, 'on route', route);
+					res.sendStatus(404);
 				}
-				else {
-					if (this.opts.debug) console.log("Failed to reach socket", socketId, "on route", route)
-					res.sendStatus(404)
-				}
+			};
 
-			}
-
-			const finalRoute = route.trim().startsWith('-') ? new RegExp(path) : `/${path}`;
-			this.app[method](finalRoute, callback)
-			this.callbacks_to_sockets.set(callback, socketId)
-			if (this.opts.debug) console.log("Registered route", finalRoute.toString(), "on method", method)
-			return callback
+			const finalRoute = route.trim().startsWith('-')
+				? new RegExp(path)
+				: `/${path}`;
+			this.app[method](finalRoute, callback);
+			this.callbacks_to_sockets.set(callback, socketId);
+			if (this.opts.debug)
+				console.log(
+					'Registered route',
+					finalRoute.toString(),
+					'on method',
+					method
+				);
+			return callback;
 		});
 
-		this.sockets_to_callbacks.set(socketId, callbacks)
+		this.sockets_to_callbacks.set(socketId, callbacks);
 	}
 
 	unRegisterRoutes(socket: Socket) {
-		this.emit("CLIENT_DISCONNECT", socket.id)
-		const callbacks = this.sockets_to_callbacks.get(socket.id) || []
-		if (callbacks.length == 0) return
+		this.emit('CLIENT_DISCONNECT', socket.id);
+		const callbacks = this.sockets_to_callbacks.get(socket.id) || [];
+		if (callbacks.length == 0) return;
 
 		for (let i = 0; i < this.app._router.stack.length; i++) {
-			const route = this.app._router.stack[i]
-
+			const route = this.app._router.stack[i];
 
 			if (!route?.route?.path) continue;
 
 			const funcHandle = route.route.stack[0].handle;
 
 			const itemIndex = callbacks.findIndex((c) => {
-				return c === funcHandle
-			})
+				return c === funcHandle;
+			});
 
 			if (itemIndex < 0) continue;
 
-			this.app._router.stack.splice(i, 1)
+			this.app._router.stack.splice(i, 1);
 
-			callbacks.splice(itemIndex, 1)
+			callbacks.splice(itemIndex, 1);
 
-			i--
+			i--;
 		}
-
 	}
 
-	async onIdentify(authenticator: (Socket: Socket, data: IdentifyType) => boolean) {
-		this.authenticator = authenticator
+	async onIdentify(
+		authenticator: (Socket: Socket, data: IdentifyType) => boolean
+	) {
+		this.authenticator = authenticator;
 	}
 
 	start(callback?: () => void) {
-
 		if (this.opts.use_ssl) {
 			if (!this.opts.ssl_cert || !this.opts.ssl_key) {
-				throw new Error(`Missing SSl Cert (and/or) Key`)
+				throw new Error(`Missing SSl Cert (and/or) Key`);
 			}
 
-			this.server = https.createServer({ key: this.opts.ssl_key, cert: this.opts.ssl_cert }, this.app);
-		}
-		else {
+			this.server = https.createServer(
+				{ key: this.opts.ssl_key, cert: this.opts.ssl_cert },
+				this.app
+			);
+		} else {
 			this.server = http.createServer(this.app);
 		}
 
 		this.socket = new SocketIoServer(this.server);
 
-		this.socket.on('error', console.log)
+		this.socket.on('error', console.log);
 		this.socket.on('connection', (socket) => {
-
 			socket.once('client-identify', async (data) => {
-				if (!this.authenticator(socket, data)) return
-				this.authenticated_sockets.set(socket.id, 1)
-				this.sockets_to_callbacks.set(socket.id, data.routes)
-				await this.registerRoutes(data.routes, socket.id)
-			})
+				if (!this.authenticator(socket, data)) return;
+				this.authenticated_sockets.set(socket.id, 1);
+				this.sockets_to_callbacks.set(socket.id, data.routes);
+				await this.registerRoutes(data.routes, socket.id);
+			});
 
 			socket.once('disconnect', () => {
-				if (!this.authenticated_sockets.get(socket.id)) return
-				this.unRegisterRoutes(socket)
-			})
+				if (!this.authenticated_sockets.get(socket.id)) return;
+				this.unRegisterRoutes(socket);
+			});
 		});
 
-		this.server.listen(this.opts.port, this.opts.hostname, undefined, callback)
+		this.server.listen(this.opts.port, this.opts.hostname, undefined, callback);
 	}
 }
-
